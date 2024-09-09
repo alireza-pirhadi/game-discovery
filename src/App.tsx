b988ad96d5482db8b5bbab6720ab95f1f3c19c54
alireza-pirhadi <alirezapirhadi@gmail.com>
2024-09-09 12:08:55 -0400

    Created Grid and NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <></>;
+  return (
+    <Grid
+      templateAreas={{
+        base: '"nav" "main"',
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="blue">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="yellow">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
