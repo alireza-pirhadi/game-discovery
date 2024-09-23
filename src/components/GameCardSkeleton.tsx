3a14ca5f364a4de63d715b415d46101caab1d121
alireza-pirhadi <alirezapirhadi@gmail.com>
2024-09-23 13:24:43 -0400

    Added game card container

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 function GameCardSkeleton() {
   return (
-    <Card width="250px" borderRadius={10} overflow="hidden">
+    <Card>
       <Skeleton height="165px" />
       <CardBody>
         <SkeletonText height="84px" />
